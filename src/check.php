5c4271c83e4fe55a1935d2d4858886baa6fa4e8c
Rahul <rahulchaudhary@cedcoss.com>
2022-04-29 12:05:01 +0530

    Last commit

--- src/check.php
+++ src/check.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION["complete_todo"])) {
 if($_SERVER["REQUEST_METHOD"]=="POST")
 foreach ($_SESSION["todo"] as $key => $value) {
 
-    if(($key == $_POST['chk_'.$key])&&($key>0)){
+    if(($key == $_POST['chk_'.$key])&&($key>=0)){
         array_push($_SESSION['complete_todo'],$value);
         array_splice($_SESSION['todo'],$key,1);
 
--- src/uncheck.php
+++ src/uncheck.php
@@ -4,7 +4,7 @@ ob_start();
 if($_SERVER["REQUEST_METHOD"]=="POST")
 foreach ($_SESSION["complete_todo"] as $key => $value) {
 
-    if(($key == $_POST['unchk_'.$key])&&($key>0)){
+    if(($key == $_POST['unchk_'.$key])&&($key>=0)){
         array_push($_SESSION['todo'],$value);
         array_splice($_SESSION['complete_todo'],$key,1);
 
